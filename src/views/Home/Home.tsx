e731e2c0ded4ae1aa6761408ca435529d4ef3e4c
tatianallorente <tatianallorente@gmail.com>
2026-03-16 21:16:22 +0100

    Minor visual changes

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,26 +1,33 @@
+import classnames from 'classnames';
 import { CardWrapper, ForecastFiveDays, LoadingOverlay } from '@/components';
 import { useController } from './Home.controller';
 
 export function Home() {
   const { data } = useController();
+
   return (
     <>
       <div className="mb-4 flex w-full max-w-full gap-4">
         <div className="flex w-3/4 flex-col gap-4">
           {/* Today */}
-          <CardWrapper title="WeatherToday" className="flex-1">
+          <CardWrapper title="Weather today" className="flex-1">
             <p>TODO: WeatherToday </p>
           </CardWrapper>
           {/* Forecast */}
           <CardWrapper title="Forecast" className="flex-1">
-            <p>TODO: Forecast </p>
+            {data.isLoading ? <LoadingOverlay /> : <p>TODO: Forecast </p>}
           </CardWrapper>
         </div>
         {/* Forecast Five Days */}
         <div className="flex w-1/4">
-          <CardWrapper title="Forecast 5 days" className="flex-1">
+          <CardWrapper
+            title="Forecast 5 days"
+            className={classnames('flex-1', {
+              'flex flex-col': data.isLoading,
+            })}
+          >
             {data.isLoading ? (
-              <div className="flex items-center justify-center">
+              <div className="flex flex-1 items-center justify-center">
                 <LoadingOverlay />
               </div>
             ) : (
