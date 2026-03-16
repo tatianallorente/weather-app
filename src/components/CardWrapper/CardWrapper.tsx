9c29eecdf7e2c409862602eedcb78cdbb34bc0a9
tatianallorente <tatianallorente@gmail.com>
2026-03-16 20:43:24 +0100

    New component CardWrapper

---
+++ src/components/CardWrapper/CardWrapper.tsx
@@ -0,0 +1,22 @@
+import { Typography } from '@mui/material';
+import type { ReactNode } from 'react';
+
+interface CardWrapperProps {
+  title?: string;
+  className?: string;
+  children: ReactNode;
+}
+
+export function CardWrapper({ title = '', className = '', children }: CardWrapperProps) {
+  return (
+    <div className={`border-secondary-dark bg-secondary rounded-lg border p-4 ${className}`}>
+      {title && (
+        <Typography variant="subtitle1" component="h4" className="text-neutral-400">
+          {title}
+        </Typography>
+      )}
+
+      {children}
+    </div>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './ApplicationLayout';
 export * from './ErrorModal';
 export * from './LoadingOverlay';
+export * from './CardWrapper';
