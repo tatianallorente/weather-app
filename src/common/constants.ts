db9020c6ac585f973d3b87c9f70bb53622b3ba06
tatianallorente <tatianallorente@gmail.com>
2026-03-21 22:21:53 +0100

    New constant with units

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -2,6 +2,13 @@ export const DRAWER_WIDTH = '125px';
 export const NAVBAR_HEIGTH = '54px';
 export const iconBaseUrl = 'https://openweathermap.org/img/wn/';
 
+export const UNITS = {
+  humidity: '%',
+  pressure: 'hPa',
+  visibility: 'km',
+  wind: 'm/s',
+};
+
 // TODO: SETTINGS
 export const city = 'madrid';
 export const units = 'metric';
